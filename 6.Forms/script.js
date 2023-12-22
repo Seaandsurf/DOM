156a72b21fcc64ac523f394fb79516fda4a5b13d
Seaandsurf <mathias.hardat@gmail.com>
2023-12-22 11:40:29 +0100

    6.Forms

--- 6.Forms/script.js
+++ 6.Forms/script.js
@@ -1,57 +1,59 @@
-// Function to update the content of the span with the first input value
-function updateFirstName() {
-    const firstNameInput = document.getElementById("first-name");
-    const displayFirstName = document.getElementById("display-firstname");
-    displayFirstName.textContent = firstNameInput.value;
-}
 
-// Function to handle age input and show/hide hard truth accordingly
-function handleAgeInput() {
-    const ageInput = document.getElementById("age");
-    const hardTruthSection = document.getElementById("a-hard-truth");
-
-    if (parseInt(ageInput.value) < 18) {
-        hardTruthSection.style.display = "none";
-    } else {
-        hardTruthSection.style.display = "block";
+    // Function to update the content of the span with the first input value
+    function updateFirstName() {
+      const firstNameInput = document.getElementById("firstname");
+      const displayFirstName = document.getElementById("display-firstname");
+      displayFirstName.textContent = firstNameInput.value;
     }
-}
-
-// Function to handle password input and validation
-function handlePasswordInput() {
-    const passwordInput = document.getElementById("password");
-    const confirmInput = document.getElementById("confirm-password");
-
-    // Visual hint for password length validation
-    if (passwordInput.value.length < 6 || passwordInput.value !== confirmInput.value) {
+  
+    // Function to handle age input and show/hide hard truth accordingly
+    function handleAgeInput() {
+      const ageInput = document.getElementById("age");
+      const hardTruthSection = document.getElementById("a-hard-truth");
+  
+      if (parseInt(ageInput.value) < 18) {
+        hardTruthSection.style.visibility = "hidden";
+      } else {
+        hardTruthSection.style.visibility = "visible";
+      }
+    }
+  
+    // Function to handle password input and validation
+    function handlePasswordInput() {
+      const passwordInput = document.getElementById("pwd");
+      const confirmInput = document.getElementById("pwd-confirm");
+  
+      // Visual hint for password length validation
+      if (passwordInput.value.length < 6 || passwordInput.value !== confirmInput.value) {
         passwordInput.style.border = "1px solid red";
         confirmInput.style.border = "1px solid red";
-    } else {
+      } else {
         passwordInput.style.border = "";
         confirmInput.style.border = "";
+      }
     }
-}
-
-// Function to toggle dark mode based on the selected option
-function toggleDarkMode() {
-    const body = document.body;
-    const select = document.getElementById("dark-mode-toggle");
-
-    if (select.value === "dark") {
+  
+    // Fonction "toogle dark mode" basé sur l'option selectionnée
+    function toggleDarkMode() {
+      const body = document.body;
+      const select = document.getElementById("toggle-darkmode");
+  
+      if (select.value === "dark") {
         body.style.backgroundColor = "black";
         body.style.color = "white";
-    } else {
+      } else {
         body.style.backgroundColor = "";
         body.style.color = "";
+      }
     }
-}
+  
+    // Add keyup listeners
+    document.getElementById("firstname").addEventListener("keyup", updateFirstName);
+    document.getElementById("age").addEventListener("keyup", handleAgeInput);
+    document.getElementById("pwd").addEventListener("keyup", handlePasswordInput);
+    document.getElementById("pwd-confirm").addEventListener("keyup", handlePasswordInput);
+  
+    // Add change listener for dark mode toggle
+    document.getElementById("toggle-darkmode").addEventListener("change", toggleDarkMode);
 
-// Add keyup listeners
-document.getElementById("first-name").addEventListener("keyup", updateFirstName);
-document.getElementById("age").addEventListener("keyup", handleAgeInput);
-document.getElementById("password").addEventListener("keyup", handlePasswordInput);
-document.getElementById("confirm-password").addEventListener("keyup", handlePasswordInput);
-
-// Add change listener for dark mode toggle
-document.getElementById("dark-mode-toggle").addEventListener("change", toggleDarkMode);
-
+  
